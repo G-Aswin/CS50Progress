b6f8297d23b7a727f8ccda7b7d2c38016e966e35
G-Aswin <G-Aswin@users.noreply.github.com>
2020-03-08 20:37:58 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -1,13 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_height(void);
+int get_height(void);                                    //function to read height
 
 int main(void)
 {
-    int h = get_height();
+    int h = get_height();                                //takes height from user
     
-    for (int i = 0; i < h; i++)
+    for (int i = 0; i < h; i++)                          //loop to print the pyramid
     {
         for(int j= h - 1 - i; j > 0; j--)
         {
