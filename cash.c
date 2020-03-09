4038d81beb1d8e0877e1701cc837a49f71c1ddb9
G-Aswin <G-Aswin@users.noreply.github.com>
2020-03-09 17:01:51 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -4,37 +4,37 @@
 
 int main(void)
 {
-    int n_coins=0, cents=0;
+    int n_coins = 0, cents = 0;
     
     float change;
-    do
+    do                                           // to reject negative inputs
     {
         change = get_float("Change owed : ");
     }
-    while(change<0);
-    cents = round(change * 100);
-    if(cents!=0)
+    while (change < 0);
+    cents = round(change * 100);                 // to round off ro nearest penny
+    if (cents != 0)
     {
-        while(cents>=25)
+        while (cents >= 25)                      // counts number of quarters
         {
-            cents-=25;
+            cents -= 25;
             n_coins++;
         }
-        while(cents>=10)
+        while (cents >= 10)                      // counts number of dimes
         {
-            cents-=10;
+            cents -= 10;
             n_coins++;
         }
-        while(cents>=5)
+        while (cents >= 5)                       // counts number of nickels
         {
-            cents-=5;
+            cents -= 5;
             n_coins++;
         }
-        while(cents>=1)
+        while (cents >= 1)                       // counts number of pennies
         {
-            cents-=1;
+            cents -= 1;
             n_coins++;
         }
     }
-    printf("%i\n", n_coins);
+    printf("%i\n", n_coins);                     // prints final number of coins 
 }
