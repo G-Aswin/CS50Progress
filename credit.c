895a509385d378215486c81dbfd7f1da1b2cd7cb
G-Aswin <G-Aswin@users.noreply.github.com>
2020-03-09 20:22:41 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -2,37 +2,50 @@
 #include <cs50.h>
 #include <math.h>
 
-int checksum(long c_no);
-int no_len(long c_no);
+int checksum(long c_no);                            // function to validate Luhn's Algorithm
+int no_len(long c_no);                              // function to return number of digits of a number
 
 int main(void)
 {
     long c_no = get_long("Number : ");
-    if(checksum(c_no)==1)
+    if (checksum(c_no) == 1)
     {
-       // printf("Entered! %i\n", no_len(c_no));
-        if(no_len(c_no) == 15)
+        if (no_len(c_no) == 15)                     // check 15 digit numbers for possible validity
         {
-            if(c_no/10000000000000 == 34 || c_no/10000000000000 == 37)
+            if (c_no / 10000000000000 == 34 || c_no / 10000000000000 == 37)
+            {
                 printf("AMEX\n");
+            }
             else
+            {
                 printf("INVALID\n");
+            }
         }
-        else if(no_len(c_no) == 16)
+        else if (no_len(c_no) == 16)                // check 16 digit numbers for possible validity
         {
-            if(c_no/100000000000000 >= 51 && c_no/100000000000000 <= 55)
+            if (c_no / 100000000000000 >= 51 && c_no / 100000000000000 <= 55)
+            {
                 printf("MASTERCARD\n");
-            else if(c_no/1000000000000000 == 4)
+            }
+            else if (c_no / 1000000000000000 == 4)
+            {
                 printf("VISA\n");
+            }
             else
+            {
                 printf("INVALID\n");
+            }
         }
-        else if(no_len(c_no) == 13)
+        else if (no_len(c_no) == 13)                // check 13 digit numbers for possible validity
         {
-            if(c_no / 1000000000000 == 4)
+            if (c_no / 1000000000000 == 4)
+            {
                 printf("VISA\n");
+            }
             else
+            {
                 printf("INVALID\n");
+            }
         }
         else
         {
@@ -45,38 +58,39 @@ int main(void)
     }
 }
 
-
-
-
-int checksum(long c_no)
+int checksum(long c_no)                                  // function to validate Luhn's Algorithm
 {
-    int s_eve=0, s_odd=0, temp=0;
-    while(c_no!=0)
+    int s_eve = 0, s_odd = 0, temp = 0;
+    while (c_no != 0)
     {
-        s_odd += c_no%10;
+        s_odd += c_no % 10;
         c_no /= 10;
-        temp = (c_no%10)*2;
-        while(temp!=0)
+        temp = (c_no % 10) * 2;
+        while (temp != 0)
         {
-            s_eve+=temp%10;
-            temp/=10;
+            s_eve += temp % 10;
+            temp /= 10;
         }
         c_no /= 10;
     }
 
-    if((s_odd+s_eve)%10==0)
+    if ((s_odd + s_eve) % 10 == 0)
+    {
         return 1;
+    }
     else
+    {
         return 0;
+    }
 }
 
-int no_len(long c_no)
+int no_len(long c_no)                                     // function to return number of digits of a number
 {
-    int len=0;
-    while(c_no!=0)
+    int len = 0;
+    while (c_no != 0)
     {
         len++;
-        c_no/=10;
+        c_no /= 10;
     }
     return len;
 }
