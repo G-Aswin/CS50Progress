b11f1c0d986ea3759c8bdba55dbbaa76d381aa37
G-Aswin <G-Aswin@users.noreply.github.com>
2020-03-21 10:59:05 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -4,43 +4,44 @@
 #include <string.h>
 #include <math.h>
 
-int count_letters( string text );
-int count_words( string text );
-int count_sentences( string text );
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
-    string text = get_string("Text: ");
-    float L = count_letters( text ) , S = count_sentences( text) ;
-    int W = count_words( text ) ;
+    string text = get_string("Text: ");                                     //reads the input text from the user
+    float L = count_letters(text), S = count_sentences(text) ;              //stores number of letters and sentences respectively
+    int W = count_words(text) ;                                             //stores number of words
 
-    L = (L * 100) / W ;
-    S = (S * 100) / W ;
+    L = (L * 100) / W ;                                                     //number of letters for 100 words
+    S = (S * 100) / W ;                                                     //number of sentences for 100 words
 
-    float index = ((0.0588 * L) - (0.296 * S) - 15.8) ;
-    index = roundf( index );
+    float index = ((0.0588 * L) - (0.296 * S) - 15.8)
+                  ;                     //calculates the index value according to Coleman-Liau Formulae
+    index = roundf(index);                                                  //rounds the index to its nearest whole number
 
-    if (index >= 16)
+    if (index >= 16)                                                        //else-if ladder to print the grades accordingly
     {
-        printf ("Grade 16+\n");
+        printf("Grade 16+\n");
     }
     else if (index < 1)
     {
-        printf ("Before Grade 1\n");
+        printf("Before Grade 1\n");
     }
     else
     {
-        printf ("Grade %.0f\n" , index);
+        printf("Grade %.0f\n", index);
     }
 }
 
 
-int count_letters( string text )
+int count_letters(string text)                                              //function to count the number of letters
 {
     int count = 0;
-    for(int i = 0 , n = strlen(text); i < n ; i++)
+    for (int i = 0, n = strlen(text); i < n ; i++)
     {
-        if(isalpha(text[i]))
+        if (isalpha(text[i]))
         {
             count++;
         }
@@ -49,26 +50,26 @@ int count_letters( string text )
 }
 
 
-int count_words( string text )
+int count_words(string text)                                                //function to count the number of words
 {
     int count = 0;
-    for(int i = 0 , n = strlen(text); i < n ; i++)
+    for (int i = 0, n = strlen(text); i < n ; i++)
     {
-        if(text[i]==' ')
+        if (text[i] == ' ')
         {
             count++;
         }
     }
-    return count+1;
+    return count + 1;
 }
 
 
-int count_sentences( string text )
+int count_sentences(string text)                                            //function to count the number of sentences
 {
     int count = 0;
-    for(int i = 0 , n = strlen(text); i < n ; i++)
+    for (int i = 0, n = strlen(text); i < n ; i++)
     {
-        if(text[i] == '.' || text[i] == '?' || text[i] == '!')
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
         {
             count++;
         }
