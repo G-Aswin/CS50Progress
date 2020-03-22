0b1b568b7ba5659e805997b64365436b710e8e5a
G-Aswin <G-Aswin@users.noreply.github.com>
2020-03-22 07:06:05 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -4,65 +4,67 @@
 #include <string.h>
 #include <math.h>
 
-int validate (int n, string arr[]);
+int validate(int n, string
+             arr[]);                                              //function to check whether input is proper and returns error code or valid key
 
 
-int main (int argc, string argv[])
+int main(int argc, string argv[])                                               //main function
 {
-    int ret,key;
-    ret = validate(argc, argv);
-    if(ret == 0)
+    int ret, key;
+    ret = validate(argc, argv);                                                 //collects return value from validate function
+    if (ret == 0)
     {
-        return 1;
+        return 1;                                                               //if invalid command-line input, program ends here
     }
     else
     {
-        key = ret;
+        key = ret;                                                              //valid key is stored here
     }
 
-    string plaintext = get_string("plaintext: ");
+    string plaintext = get_string("plaintext: ");                               //asks user for the plain text
     printf("ciphertext: ");
     for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
         if (islower(plaintext[i]) != 0)
         {
-            printf ("%c", ((((int)plaintext[i] - 96)+ key) % 26) + 96);
+            printf("%c", ((((int)plaintext[i] - 96) + key) % 26) + 96);         //encrypts the lower case alphabets according to the key
         }
         else if (isupper(plaintext[i]) != 0)
         {
-            printf ("%c", ((((int)plaintext[i] - 64)+ key) % 26) + 64);
+            printf("%c", ((((int)plaintext[i] - 64) + key) % 26) + 64);         //encrypts the upper case alphabets according to the key
         }
         else
         {
-            printf("%c", plaintext[i]);
+            printf("%c", plaintext[i]);                                         //prints as it is if its not an alphabet
         }
     }
     printf("\n");
 }
 
-int validate (int n, string arr[])
+int validate(int n, string
+             arr[])                                               //function to check if command-line input is valid
 {
-    if(n != 2)
+    if (n != 2)                                                                 //returns error if more than one command-line input
     {
         printf("Usage: ./caesar key\n");
         return 0;
     }
     else
     {
-        int key=0, pos=0;
-        for(int i = strlen(arr[1]) - 1; i >= 0; i--)
+        int key = 0, pos = 0;
+        for (int i = strlen(arr[1]) - 1; i >= 0; i--)                           //extracts the input from the array one-by-one
         {
-            if(isdigit(arr[1][i]) == 0)
+            if (isdigit(arr[1][i]) == 0)                                        //checks if the array element is indeed a digit
             {
-                printf ("Usage: ./caesar key\n");
+                printf("Usage: ./caesar key\n");
                 return 0;
             }
             else
             {
-                key += (arr[1][i] - 48)*pow(10, pos);
+                key += (arr[1][i] - 48) * pow(10, pos);                         //forms the final key to be returned back
                 pos++;
             }
         }
-        return key;
+        return key;                                                             //returns the input key
     }
 }
