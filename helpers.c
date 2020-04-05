b6e0b1915b2174ab7f4f92245fbecf58aa0682e3
G-Aswin <G-Aswin@users.noreply.github.com>
2020-04-05 12:46:23 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -8,7 +8,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            float average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3;
+            float average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3.0;
         /*   if (average % 1 <= 0.5)
             {
                 average -= average % 1;
