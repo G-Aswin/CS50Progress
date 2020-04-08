474ad8a3d008f6c8e4b90cdb32181df0d0255967
G-Aswin <G-Aswin@users.noreply.github.com>
2020-04-08 06:01:33 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -4,11 +4,15 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    //reading individual pixels
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
-            float average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3.0;
+            //calculating average of all the colours
+            float average = (image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0;
+            
+            ///rounding off to nearest whole number and saving in the pixel
             average = roundf(average);
             image[i][j].rgbtBlue = image[i][j].rgbtGreen = image[i][j].rgbtRed = average;
         }
@@ -19,13 +23,17 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    //reading individual pixels
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
+            //saving the new sepia values and rounding off to nearest whole number
             float newred = roundf((.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue));
             float newgreen = roundf((.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue));
             float newblue = roundf((.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue));
+            
+            //forcing the limit to be 255
             if (newred > 255)
             {
                 newred = 255;
@@ -38,7 +46,8 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             {
                 newblue = 255;
             }
-
+            
+            //saving the new colour values
             image[i][j].rgbtRed = newred;
             image[i][j].rgbtGreen = newgreen;
             image[i][j].rgbtBlue = newblue;
@@ -50,10 +59,12 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    //loop to read all left half pixels only
     for (int i = 0; i < height; i++)
     {
-        for (int j = 0; j < width/2; j++)
+        for (int j = 0; j < width / 2; j++)
         {
+            //swapping the pixel with its horizontal mirror position
             RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][width - j - 1];
             image[i][width - j - 1] = temp;
@@ -65,8 +76,10 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    //new array to store original copy of the image
     RGBTRIPLE copyarr[height][width];
 
+    //copies the image into the new variable
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
@@ -74,16 +87,23 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             copyarr[i][j] = image[i][j];
         }
     }
+    
+    //reads each pixel
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
+            //new variables to store average of the neighbouring pixels
             int sumboxrgbtRed = 0, sumboxrgbtGreen = 0, sumboxrgbtBlue = 0;
             int sumcount = 0;
+            
+            //create the limits for the neighbouring pixels including corner and edges
             int negrow = (i == 0) ? (0) : (i - 1);
             int posrow = (i == height - 1) ? (height - 1) : (i + 1);
             int negcol = (j == 0) ? (0) : (j - 1);
             int poscol = (j == width - 1) ? (width - 1) : (j + 1);
+            
+            //opens the loop for neighbouring pixels
             for (int p = negrow; p <= posrow; p++)
             {
                 for (int q = negcol; q <= poscol; q++)
@@ -94,6 +114,8 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     sumcount++;
                 }
             }
+            
+            //calculate average and store it in the original image
             float redavg = roundf(sumboxrgbtRed / (float)sumcount);
             float greenavg = roundf(sumboxrgbtGreen / (float)sumcount);
             float blueavg = roundf(sumboxrgbtBlue / (float)sumcount);
